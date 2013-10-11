4b87abba1aa7285ef1c5c4cbc55e8ee817028b4c
Chris Zempel <czempel@Bredons-Mac-Pro.local>
2013-10-11 16:36:22 -0500

    added delete button to tasks

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -34,6 +34,12 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to root_path, notice: "List deleted"
+  end
+
   private
   
   def list_params
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,6 +20,13 @@ class TasksController < ApplicationController
     redirect_to list_path(@list)
   end
 
+  def destroy
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.find(params[:id])
+    @task.destroy
+    redirect_to list_path(@list)
+  end
+
   private
 
     def task_params
